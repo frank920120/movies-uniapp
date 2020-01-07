21189f11c26aa29ccdfe9401048dc627cc2cb09c
Frank Fang <frankfang2014@hotmail.com>
2020-01-06 17:55:29 -0800

    first commit

---
+++ App.vue
@@ -0,0 +1,21 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launcn heresadasdasdasdasdas dasdasdasd');
+			console.log('App Launcn heresadasdasdasdasdas dasdasdasd')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		},
+		created(){
+			
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
