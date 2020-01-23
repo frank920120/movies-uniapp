18c55f6e4937865628f120936ca90243d85c6d91
Frank Fang <frankfang2014@hotmail.com>
2020-01-22 23:33:14 -0800

    自定义预览封面图

---
+++ pages/cover/cover.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class='black'>
+		<image :src='imageUrl' class='cover' mode='widthFix'></image>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				imageUrl: null
+			}
+		},
+		methods: {},
+		onLoad(params) {
+			console.log(params)
+			uni.setNavigationBarColor({
+				frontColor: '#ffffff',
+				backgroundColor: '#000000'
+			})
+			this.imageUrl = params.cover
+		}
+	}
+</script>
+<style>
+	.black {
+		background-color: #000000;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		position: fixed;
+		z-index: 0;
+	}
+	.cover{
+		z-index: 999;
+	}
+	
+</style>
