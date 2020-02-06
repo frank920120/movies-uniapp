ff55cd3b9a287779e348a8f62689dfc13cd24057
Frank Fang <frankfang2014@hotmail.com>
2020-02-05 20:37:39 -0800

    用户登陆错误和方法挂载

--- main.js
+++ main.js
@@ -4,7 +4,14 @@ import App from './App'
 Vue.config.productionTip = false
 // Vue.prototype.url = 'https://www.imovietrailer.com/superhero/'
 App.mpType = 'app'
-
+Vue.prototype.getGolbalUser = function(key){
+		let userInfo = uni.getStorageSync(key);
+		if(userInfo!= null && userInfo!= undefined && userInfo!=''){
+			return userInfo
+	}else{
+		return null
+	}
+}
 const app = new Vue({
     ...App
 })
